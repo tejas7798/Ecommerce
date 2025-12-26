beb741f372825dc7d7ca3e4fcf7caca2bede6662
Tejas Patle <tejaspatle77@gmail.com>
2025-12-26 15:28:51 +0530

    FronteEnd Ecommerce

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
      getUserName: "https://ecommerce-backend-plum-rho.vercel.app/cart/:username",
-     getProductById : "https://ecommerce-backend-plum-rho.vercel.app/products/:id",
+     getProductById : "https://ecommerce-backend-plum-rho.vercel.app/api/products/:id",
      deleteProductsFromCart : "https://ecommerce-backend-plum-rho.vercel.app/cart/deleteCartItem",
      getProducts : "https://ecommerce-backend-plum-rho.vercel.app/api/products",
      addToCart : "https://ecommerce-backend-plum-rho.vercel.app/cart/addToCart",
