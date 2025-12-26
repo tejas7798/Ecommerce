ac4887a37d6ac99b8d20184ff8857637d2bc8b7a
Tejas Patle <tejaspatle77@gmail.com>
2025-12-26 14:15:47 +0530

    FronteEnd Ecommerce

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
-     getUserName: "https://ecommerce-backend-plum-rho.vercel.app/cart/:username",
-     getProductById : "https://ecommerce-backend-plum-rho.vercel.app/api/products/:id",
-     deleteProductsFromCart : "https://ecommerce-backend-plum-rho.vercel.app/cart/deleteCartItem",
-     getProducts : "https://ecommerce-backend-plum-rho.vercel.app/api/products",
-     addToCart : "https://ecommerce-backend-plum-rho.vercel.app/cart/addToCart",
-     getToken : "https://ecommerce-backend-plum-rho.vercel.app/get/token",
-     userLogin : "https://ecommerce-backend-plum-rho.vercel.app/app/login",
-     userRegister : "https://ecommerce-backend-plum-rho.vercel.app/app/login/register"
+     getUserName: "http://localhost:8080/cart/:username",
+     getProductById : "http://localhost:8080/api/products/:id",
+     deleteProductsFromCart : "http://localhost:8080/cart/deleteCartItem",
+     getProducts : "http://localhost:8080/api/products",
+     addToCart : "http://localhost:8080/cart/addToCart",
+     getToken : "http://localhost:8080/get/token",
+     userLogin : "http://localhost:8080/app/login",
+     userRegister : "http://localhost:8080/app/login/register"
 }
